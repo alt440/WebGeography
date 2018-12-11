8ff49588798620b2b51b6f5d8de963f541ec6653
bob <alexandre.therrien3@gmail.com>
2018-12-10 23:13:23 -0500

    Added different arrays based on the continent the user chose to focus on

--- js/questions.js
+++ js/questions.js
@@ -40,12 +40,105 @@
                 'tokelau','tonga','tuvalu','vanuatu','wallis-and-futuna-islands',
                 'western-samoa'];
 
+  //these arrays below are specifically for versions of the game based on a single continent
+  //countries only from africa
+  countriesAfrica = ['algeria','angola','benin','botswana','burkina-faso','burundi',
+                'cameroon','central-african-republic','chad','cote-d_ivory',
+                'democratic-republic-of-congo','djibouti','egypt','equatorial-guinea',
+                'eritrea','ethiopia','gabon','ghana','guinea','guinea-bissau',
+                'kenya','lesotho','liberia','libya','madagascar','malawi',
+                'mali','mauritania','morocco','mozambique','namibia','niger',
+                'nigeria','republic-of-congo','rwanda','senegal','sierra-leone',
+                'somalia','south-africa','south-sudan','sudan','swaziland',
+                'tanzania','the-gambia','togo','tunisia','uganda','western-sahara',
+                'zambia','zimbabwe'];
+
+  //countries only from asia
+  countriesAsia = ['afghanistan','armenia','azerbaijan','bangladesh',
+  'bhutan','brunei','buhrain','cambodia','china','east-timor','georgia',
+  'india','indonesia','iran','iraq','israel','japan','jordan',
+  'kazhakstan','kuwait','kyrgyzstan','laos','lebanon','malaysia',
+  'mongolia','myanmar','nepal','north-korea','oman','pakistan',
+  'philippines','qatar','russia','saudi-arabia','singapore','south-korea',
+  'syria','taiwan','tajikistan','thailand','turkmenistan',
+  'united-arab-emirates','uzbekistan','vietnam','yemen'];
+
+  //countries only from europe
+  countriesEurope = ['albania',
+  'austria','belarus','belgium','bosnia','bulgaria','croatia',
+  'czech-republic','denmark','estonia','finland','france','germany',
+  'greece','hungary','iceland','ireland','italy','kosovo','latvia',
+  'lithuania','macedonia','moldovia','montenegro','netherlands',
+  'norway','poland','portugal','romania','serbia','slovakia','slovenia',
+  'spain','sweden','switzerland','turkey','ukraine','united-kingdom'];
+
+  //countries only from north america
+  countriesNorthAmerica = ['bahamas','belize','canada','costa-rica','cuba','dominican-republic',
+  'el-salvador','greenland','guatemala','haiti','honduras','jamaica',
+  'mexico','nicaragua','panama','puerto-rico','united-states'];
+
+  //countries only from south america
+  countriesSouthAmerica = ['argentina','bolivia','brazil','chile','columbia','ecuador',
+  'falkland-islands','french-guiana','guyana','paraguay','peru',
+  'suriname','uruguay','venezuela'];
+
+  //countries only from oceania
+  countriesOceania = ['american-samoa','australia',
+  'cook-islands','coral-sea-islands','federated-states-of-micronesia',
+  'fiji','french-polynesia','guam','kiribati','marshall-islands',
+  'nauru','new-caledonia','new-zealand','niue','northern-mariana-islands',
+  'palau','papua-new-guinea','pitcaim-islands','solomon-islands',
+  'tokelau','tonga','tuvalu','vanuatu','wallis-and-futuna-islands',
+  'western-samoa'];
+
   //chooses the source of the next image. This generates a new image
   function chooseSource(){
-    var value = Math.round(Math.random()*(countries.length-1));
+    //from selectContinentForPlay.js
+    var choiceContinent = localStorage.getItem("continentChoice");
+    console.log(localStorage.getItem("continentChoice"));
+    var value;
+    if(choiceContinent == undefined || choiceContinent == 0){
+      value = Math.round(Math.random()*(countries.length-1));
+      console.log("img/countries/"+countries[value]+".jpg"+counterRightAnswers+" "+value);
+      return "img/countries/"+countries[value]+".jpg";
+    }
 
-    console.log("img/countries/"+countries[value]+".jpg"+counterRightAnswers+" "+value);
-    return "img/countries/"+countries[value]+".jpg";
+    else if(choiceContinent == 1){
+      value = Math.round(Math.random()*(countriesAfrica.length-1));
+      console.log("img/countries/"+countriesAfrica[value]+".jpg"+counterRightAnswers+" "+value);
+      return "img/countries/"+countriesAfrica[value]+".jpg";
+    }
+
+    else if(choiceContinent == 2){
+      value = Math.round(Math.random()*(countriesAsia.length-1));
+      console.log("img/countries/"+countriesAsia[value]+".jpg"+counterRightAnswers+" "+value);
+      return "img/countries/"+countriesAsia[value]+".jpg";
+    }
+
+    else if(choiceContinent == 3){
+      value = Math.round(Math.random()*(countriesEurope.length-1));
+      console.log("img/countries/"+countriesEurope[value]+".jpg"+counterRightAnswers+" "+value);
+      return "img/countries/"+countriesEurope[value]+".jpg";
+    }
+
+    else if(choiceContinent == 4){
+      value = Math.round(Math.random()*(countriesNorthAmerica.length-1));
+      console.log("img/countries/"+countriesNorthAmerica[value]+".jpg"+counterRightAnswers+" "+value);
+      return "img/countries/"+countriesNorthAmerica[value]+".jpg";
+    }
+
+    else if(choiceContinent == 5){
+      value = Math.round(Math.random()*(countriesOceania.length-1));
+      console.log("img/countries/"+countriesOceania[value]+".jpg"+counterRightAnswers+" "+value);
+      return "img/countries/"+countriesOceania[value]+".jpg";
+    }
+
+    else if(choiceContinent == 6){
+      value = Math.round(Math.random()*(countriesSouthAmerica.length-1));
+      console.log("img/countries/"+countriesSouthAmerica[value]+".jpg"+counterRightAnswers+" "+value);
+      return "img/countries/"+countriesSouthAmerica[value]+".jpg";
+    }
+
   }
 
   //checks if the right answer was inputted.
