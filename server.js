5112cc6453e94941301ff70b1b68d01b89281d32
bob <alexandre.therrien3@gmail.com>
2018-12-16 20:43:37 -0500

    Made connection with MongoDB database from mlab

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
 var express = require('express');//our pre-built server
 var bodyParser = require('body-parser');//used to get POST info
 
+//MONGODB from mlab website connection
+//import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+
+//(Focus on This Variable)
+var url = "mongodb://perS0nADm1N:"+encodeURIComponent("*geo@P0w3r3d*")+"@ds155097.mlab.com:55097/web_geography";
+//(Focus on This Variable)
+
+// Use connect method to connect to the Server
+  MongoClient.connect(url, {useNewUrlParser:true}, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
+//END MONGODB connection
+
 var path = require('path');
 
 var app = express();
