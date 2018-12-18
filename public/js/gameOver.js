202fcdcd244a8a51e283f8062561fc0fc6774f7f
bob <alexandre.therrien3@gmail.com>
2018-12-17 23:12:32 -0500

    Separated script from html

---
+++ public/js/gameOver.js
@@ -0,0 +1,27 @@
+
+  var counter = localStorage.getItem("counterRightAnswers");
+
+  //the source of the image of the country that the user failed to input correctly
+  var sourceLastImg = localStorage.getItem("lastCountry");
+  //spliting it correctly to get only the name of the country
+  console.log(sourceLastImg);
+  var nameImage = sourceLastImg.split("/");
+  var onlyName = nameImage[2];
+  onlyName = onlyName.split(".")[0];
+  onlyName = onlyName.split("-").join(" ");
+  onlyName = onlyName.replace("_","'");
+
+  $(document).ready(function(){
+    if(counter!=undefined){
+      $('#losing_message').text('You were able to visit '+
+                        counter+' countries!');
+      $('#country_last_asked').text('You failed when you were asked for the country '+onlyName+'.');
+    }
+    $('#yes').click(function(){
+      window.location.href = "questions.html";
+    })
+
+    $('#no').click(function(){
+      window.location.href = "homePage.html";
+    })
+  });
---
+++ public/js/play.js
@@ -0,0 +1,36 @@
+//in milliseconds, time before elements appear
+var timeDelay = 1000;
+//time given for animation
+var timeAnimation = 800;
+$(document).ready(function(){
+
+  //hides the text and buttons before displaying them
+  $('#firstText').hide();
+  $('#secondText').hide();
+  $('#buttons').hide();
+
+  //put it at faster time delay, because we dont want user to wait that long
+  setTimeout(function(){
+    $('#firstText').fadeIn(timeAnimation);
+  },timeDelay)
+
+  //put *2 for timeDelay because this element needs to appear after the first text
+  setTimeout(function(){
+    $('#secondText').fadeIn(timeAnimation);
+  }, timeDelay*2)
+
+  //put *3 for timeDelay because this element needs to appear after the second text
+  setTimeout(function(){
+    $('#buttons').fadeIn(timeAnimation);
+  }, timeDelay*2 +500)
+
+
+  //put functions for buttons
+  $('#no').click(function(){
+    window.location.href = "homePage.html";
+  })
+
+  $('#yes').click(function(){
+    window.location.href = "typeQuestion.html";
+  })
+});
