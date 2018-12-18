1265c3453b947d51a04769858601f5a4ec591581
bob <alexandre.therrien3@gmail.com>
2018-12-18 09:37:14 -0500

    Separated lists in questions.js into a different file

---
+++ public/js/countryLists.js
@@ -0,0 +1,87 @@
+//list of all the countries, which are the names of the images for reference
+countries = ['algeria','angola','benin','botswana','burkina-faso','burundi',
+              'cameroon','central-african-republic','chad','cote-d_ivory',
+              'democratic-republic-of-congo','djibouti','egypt','equatorial-guinea',
+              'eritrea','ethiopia','gabon','ghana','guinea','guinea-bissau',
+              'kenya','lesotho','liberia','libya','madagascar','malawi',
+              'mali','mauritania','morocco','mozambique','namibia','niger',
+              'nigeria','republic-of-congo','rwanda','senegal','sierra-leone',
+              'somalia','south-africa','south-sudan','sudan','swaziland',
+              'tanzania','the-gambia','togo','tunisia','uganda','western-sahara',
+              'zambia','zimbabwe','afghanistan','armenia','azerbaijan','bangladesh',
+              'bhutan','brunei','buhrain','cambodia','china','east-timor','georgia',
+              'india','indonesia','iran','iraq','israel','japan','jordan',
+              'kazhakstan','kuwait','kyrgyzstan','laos','lebanon','malaysia',
+              'mongolia','myanmar','nepal','north-korea','oman','pakistan',
+              'philippines','qatar','russia','saudi-arabia','singapore','south-korea',
+              'syria','taiwan','tajikistan','thailand','turkmenistan',
+              'united-arab-emirates','uzbekistan','vietnam','yemen','albania',
+              'austria','belarus','belgium','bosnia','bulgaria','croatia',
+              'czech-republic','denmark','estonia','finland','france','germany',
+              'greece','hungary','iceland','ireland','italy','kosovo','latvia',
+              'lithuania','macedonia','moldovia','montenegro','netherlands',
+              'norway','poland','portugal','romania','serbia','slovakia','slovenia',
+              'spain','sweden','switzerland','turkey','ukraine','united-kingdom',
+              'bahamas','belize','canada','costa-rica','cuba','dominican-republic',
+              'el-salvador','greenland','guatemala','haiti','honduras','jamaica',
+              'mexico','nicaragua','panama','puerto-rico','united-states',
+              'argentina','bolivia','brazil','chile','columbia','ecuador',
+              'falkland-islands','french-guiana','guyana','paraguay','peru',
+              'suriname','uruguay','venezuela','american-samoa','australia',
+              'cook-islands','coral-sea-islands','federated-states-of-micronesia',
+              'fiji','french-polynesia','guam','kiribati','marshall-islands',
+              'nauru','new-caledonia','new-zealand','niue','northern-mariana-islands',
+              'palau','papua-new-guinea','pitcaim-islands','solomon-islands',
+              'tokelau','tonga','tuvalu','vanuatu','wallis-and-futuna-islands',
+              'western-samoa'];
+
+//these arrays below are specifically for versions of the game based on a single continent
+//countries only from africa
+countriesAfrica = ['algeria','angola','benin','botswana','burkina-faso','burundi',
+              'cameroon','central-african-republic','chad','cote-d_ivory',
+              'democratic-republic-of-congo','djibouti','egypt','equatorial-guinea',
+              'eritrea','ethiopia','gabon','ghana','guinea','guinea-bissau',
+              'kenya','lesotho','liberia','libya','madagascar','malawi',
+              'mali','mauritania','morocco','mozambique','namibia','niger',
+              'nigeria','republic-of-congo','rwanda','senegal','sierra-leone',
+              'somalia','south-africa','south-sudan','sudan','swaziland',
+              'tanzania','the-gambia','togo','tunisia','uganda','western-sahara',
+              'zambia','zimbabwe'];
+
+//countries only from asia
+countriesAsia = ['afghanistan','armenia','azerbaijan','bangladesh',
+'bhutan','brunei','buhrain','cambodia','china','east-timor','georgia',
+'india','indonesia','iran','iraq','israel','japan','jordan',
+'kazhakstan','kuwait','kyrgyzstan','laos','lebanon','malaysia',
+'mongolia','myanmar','nepal','north-korea','oman','pakistan',
+'philippines','qatar','russia','saudi-arabia','singapore','south-korea',
+'syria','taiwan','tajikistan','thailand','turkmenistan',
+'united-arab-emirates','uzbekistan','vietnam','yemen'];
+
+//countries only from europe
+countriesEurope = ['albania',
+'austria','belarus','belgium','bosnia','bulgaria','croatia',
+'czech-republic','denmark','estonia','finland','france','germany',
+'greece','hungary','iceland','ireland','italy','kosovo','latvia',
+'lithuania','macedonia','moldovia','montenegro','netherlands',
+'norway','poland','portugal','romania','serbia','slovakia','slovenia',
+'spain','sweden','switzerland','turkey','ukraine','united-kingdom'];
+
+//countries only from north america
+countriesNorthAmerica = ['bahamas','belize','canada','costa-rica','cuba','dominican-republic',
+'el-salvador','greenland','guatemala','haiti','honduras','jamaica',
+'mexico','nicaragua','panama','puerto-rico','united-states'];
+
+//countries only from south america
+countriesSouthAmerica = ['argentina','bolivia','brazil','chile','columbia','ecuador',
+'falkland-islands','french-guiana','guyana','paraguay','peru',
+'suriname','uruguay','venezuela'];
+
+//countries only from oceania
+countriesOceania = ['american-samoa','australia',
+'cook-islands','coral-sea-islands','federated-states-of-micronesia',
+'fiji','french-polynesia','guam','kiribati','marshall-islands',
+'nauru','new-caledonia','new-zealand','niue','northern-mariana-islands',
+'palau','papua-new-guinea','pitcaim-islands','solomon-islands',
+'tokelau','tonga','tuvalu','vanuatu','wallis-and-futuna-islands',
+'western-samoa'];
--- public/js/questions.js
+++ public/js/questions.js
@@ -3,96 +3,11 @@
   //local storage to maintain consistency between scripts. counts the number of right answers
   localStorage.setItem("counterRightAnswers", counterRightAnswers);
 
-  //list of all the countries, which are the names of the images for reference
-  countries = ['algeria','angola','benin','botswana','burkina-faso','burundi',
-                'cameroon','central-african-republic','chad','cote-d_ivory',
-                'democratic-republic-of-congo','djibouti','egypt','equatorial-guinea',
-                'eritrea','ethiopia','gabon','ghana','guinea','guinea-bissau',
-                'kenya','lesotho','liberia','libya','madagascar','malawi',
-                'mali','mauritania','morocco','mozambique','namibia','niger',
-                'nigeria','republic-of-congo','rwanda','senegal','sierra-leone',
-                'somalia','south-africa','south-sudan','sudan','swaziland',
-                'tanzania','the-gambia','togo','tunisia','uganda','western-sahara',
-                'zambia','zimbabwe','afghanistan','armenia','azerbaijan','bangladesh',
-                'bhutan','brunei','buhrain','cambodia','china','east-timor','georgia',
-                'india','indonesia','iran','iraq','israel','japan','jordan',
-                'kazhakstan','kuwait','kyrgyzstan','laos','lebanon','malaysia',
-                'mongolia','myanmar','nepal','north-korea','oman','pakistan',
-                'philippines','qatar','russia','saudi-arabia','singapore','south-korea',
-                'syria','taiwan','tajikistan','thailand','turkmenistan',
-                'united-arab-emirates','uzbekistan','vietnam','yemen','albania',
-                'austria','belarus','belgium','bosnia','bulgaria','croatia',
-                'czech-republic','denmark','estonia','finland','france','germany',
-                'greece','hungary','iceland','ireland','italy','kosovo','latvia',
-                'lithuania','macedonia','moldovia','montenegro','netherlands',
-                'norway','poland','portugal','romania','serbia','slovakia','slovenia',
-                'spain','sweden','switzerland','turkey','ukraine','united-kingdom',
-                'bahamas','belize','canada','costa-rica','cuba','dominican-republic',
-                'el-salvador','greenland','guatemala','haiti','honduras','jamaica',
-                'mexico','nicaragua','panama','puerto-rico','united-states',
-                'argentina','bolivia','brazil','chile','columbia','ecuador',
-                'falkland-islands','french-guiana','guyana','paraguay','peru',
-                'suriname','uruguay','venezuela','american-samoa','australia',
-                'cook-islands','coral-sea-islands','federated-states-of-micronesia',
-                'fiji','french-polynesia','guam','kiribati','marshall-islands',
-                'nauru','new-caledonia','new-zealand','niue','northern-mariana-islands',
-                'palau','papua-new-guinea','pitcaim-islands','solomon-islands',
-                'tokelau','tonga','tuvalu','vanuatu','wallis-and-futuna-islands',
-                'western-samoa'];
-
-  //these arrays below are specifically for versions of the game based on a single continent
-  //countries only from africa
-  countriesAfrica = ['algeria','angola','benin','botswana','burkina-faso','burundi',
-                'cameroon','central-african-republic','chad','cote-d_ivory',
-                'democratic-republic-of-congo','djibouti','egypt','equatorial-guinea',
-                'eritrea','ethiopia','gabon','ghana','guinea','guinea-bissau',
-                'kenya','lesotho','liberia','libya','madagascar','malawi',
-                'mali','mauritania','morocco','mozambique','namibia','niger',
-                'nigeria','republic-of-congo','rwanda','senegal','sierra-leone',
-                'somalia','south-africa','south-sudan','sudan','swaziland',
-                'tanzania','the-gambia','togo','tunisia','uganda','western-sahara',
-                'zambia','zimbabwe'];
-
-  //countries only from asia
-  countriesAsia = ['afghanistan','armenia','azerbaijan','bangladesh',
-  'bhutan','brunei','buhrain','cambodia','china','east-timor','georgia',
-  'india','indonesia','iran','iraq','israel','japan','jordan',
-  'kazhakstan','kuwait','kyrgyzstan','laos','lebanon','malaysia',
-  'mongolia','myanmar','nepal','north-korea','oman','pakistan',
-  'philippines','qatar','russia','saudi-arabia','singapore','south-korea',
-  'syria','taiwan','tajikistan','thailand','turkmenistan',
-  'united-arab-emirates','uzbekistan','vietnam','yemen'];
-
-  //countries only from europe
-  countriesEurope = ['albania',
-  'austria','belarus','belgium','bosnia','bulgaria','croatia',
-  'czech-republic','denmark','estonia','finland','france','germany',
-  'greece','hungary','iceland','ireland','italy','kosovo','latvia',
-  'lithuania','macedonia','moldovia','montenegro','netherlands',
-  'norway','poland','portugal','romania','serbia','slovakia','slovenia',
-  'spain','sweden','switzerland','turkey','ukraine','united-kingdom'];
-
-  //countries only from north america
-  countriesNorthAmerica = ['bahamas','belize','canada','costa-rica','cuba','dominican-republic',
-  'el-salvador','greenland','guatemala','haiti','honduras','jamaica',
-  'mexico','nicaragua','panama','puerto-rico','united-states'];
-
-  //countries only from south america
-  countriesSouthAmerica = ['argentina','bolivia','brazil','chile','columbia','ecuador',
-  'falkland-islands','french-guiana','guyana','paraguay','peru',
-  'suriname','uruguay','venezuela'];
-
-  //countries only from oceania
-  countriesOceania = ['american-samoa','australia',
-  'cook-islands','coral-sea-islands','federated-states-of-micronesia',
-  'fiji','french-polynesia','guam','kiribati','marshall-islands',
-  'nauru','new-caledonia','new-zealand','niue','northern-mariana-islands',
-  'palau','papua-new-guinea','pitcaim-islands','solomon-islands',
-  'tokelau','tonga','tuvalu','vanuatu','wallis-and-futuna-islands',
-  'western-samoa'];
-
   //chooses the source of the next image. This generates a new image
   function chooseSource(){
+
+    //from typeQuestion.js
+    var choiceTypeGame = localStorage.getItem("typeQuestion");
     //from selectContinentForPlay.js
     //localStorage stores information from different files
     var choiceContinent = localStorage.getItem("continentChoice");
@@ -101,6 +16,9 @@
 
     //below is a set of if conditions to choose the right countries depending on the choice the user made
     if(choiceContinent == undefined || choiceContinent == 0){
+
+      //condition to know if it is an exam
+      if(choiceTypeGame==1){}
       value = Math.round(Math.random()*(countries.length-1));
       console.log("img/countries/"+countries[value]+".jpg"+counterRightAnswers+" "+value);
       return "img/countries/"+countries[value]+".jpg";
@@ -131,6 +49,11 @@
     }
 
     else if(choiceContinent == 5){
+
+      //condition to know if it is an exam
+      if(choiceTypeGame==1){
+
+      }
       value = Math.round(Math.random()*(countriesOceania.length-1));
       console.log("img/countries/"+countriesOceania[value]+".jpg"+counterRightAnswers+" "+value);
       return "img/countries/"+countriesOceania[value]+".jpg";
