a550928c1c631a174a3d21a3e39968ffb96a09be
bob <alexandre.therrien3@gmail.com>
2019-01-06 20:53:49 -0500

    Added necessity of being logged in to access

--- webFlow.js
+++ webFlow.js
@@ -81,7 +81,7 @@ router.get('/flagQuestions.html', ensureAuthenticated, function(req, res){
   res.render('flagQuestions');
 });
 
-router.get('/userStats.html', function(req, res){
+router.get('/userStats.html', ensureAuthenticated, function(req, res){
 
   var message = "";
   //mongoose search
@@ -103,7 +103,7 @@ router.get('/userStats.html', function(req, res){
 
 });
 
-router.get('/statsB4ScoreSend.html', function(req, res){
+router.get('/statsB4ScoreSend.html', ensureAuthenticated, function(req, res){
   res.render('statsB4ScoreSend');
 });
 
